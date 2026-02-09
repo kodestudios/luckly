eb2dc49b6e3244417340a5e8979208f65f8b2cb4
luckly <james.cortez@eli.ans.edu.ni>
2026-02-09 15:53:06 -0600

    Update auth.js

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,23 +1,18 @@
+// assets/js/auth.js
 import { auth } from "./firebase.js";
 import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signOut,
-  onAuthStateChanged
-} from "./firebase-auth-proxy.js";
+  onAuthStateChanged,
+  signOut
+} from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
 
-export function watchAuth(cb){
-  return onAuthStateChanged(auth, cb);
+export function watchAuth(cb) {
+  console.log("[AUTH] watchAuth registered");
+  return onAuthStateChanged(auth, (user) => {
+    console.log("[AUTH] state:", user);
+    cb(user);
+  });
 }
 
-export async function registerAuth(email, password){
-  return createUserWithEmailAndPassword(auth, email, password);
-}
-
-export async function loginAuth(email, password){
-  return signInWithEmailAndPassword(auth, email, password);
-}
-
-export async function logoutAuth(){
+export async function logoutAuth() {
   return signOut(auth);
 }
