25c5a2e00a6b683352d8e415a87024cb0e7d16ed
luckly <james.cortez@eli.ans.edu.ni>
2026-02-09 14:25:55 -0600

    Create badges.manifest.js

---
+++ assets/js/badges/badges.manifest.js
@@ -0,0 +1,17 @@
+export const BADGES = [
+  {
+    key: "diamond",
+    name: "Diamond",
+    img: "assets/badges/diamond.png"
+  },
+  {
+    key: "star",
+    name: "Star",
+    img: "assets/badges/star.png"
+  },
+  {
+    key: "crown",
+    name: "Crown",
+    img: "assets/badges/crown.png"
+  }
+];
