9d82f6b98d869744662ea7db5c673b2d89881bb8
luckly <james.cortez@eli.ans.edu.ni>
2026-02-09 16:36:55 -0600

    Update badges.manifest.js

--- assets/js/badges/badges.manifest.js
+++ assets/js/badges/badges.manifest.js
@@ -2,16 +2,11 @@ export const BADGES = [
   {
     key: "diamond",
     name: "Diamond",
-    img: "assets/badges/diamond.png"
+    img: "./assets/badges/diamond.png"
   },
   {
     key: "star",
     name: "Star",
-    img: "assets/badges/star.png"
-  },
-  {
-    key: "crown",
-    name: "Crown",
-    img: "assets/badges/crown.png"
+    img: "./assets/badges/star.png"
   }
 ];
